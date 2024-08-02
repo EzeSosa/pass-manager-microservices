d09d92a0e269bce9ea122ace2a50a42657939bb3
EzeSosa <ezequielsosa423@gmail.com>
2024-08-02 17:31:25 -0300

    init: password service

---
+++ password-service/src/test/java/com/esosa/password_service/PasswordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esosa.password_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
