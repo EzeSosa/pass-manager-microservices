b3d10bf1d16dc690fa08b9a337fd06414b9098f7
EzeSosa <ezequielsosa423@gmail.com>
2024-08-02 17:14:02 -0300

    init: user service

---
+++ user-service/src/main/java/com/esosa/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.esosa.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+}
